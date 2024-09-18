f7fcd9e8d830a489cafee501f01f2e179969a3dd
Praveen Mewada <pravinmewada1999@gmail.com>
2024-09-18 16:06:37 +0530

    logout2

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -9,9 +9,12 @@ const Logout = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
+    localStorage.removeItem('token');
     sessionStorage.removeItem('token'); 
     sessionStorage.removeItem('id');
-    sessionStorage.removeItem('name');  // Clear the token from localStorage
+    localStorage.removeItem('id');
+    sessionStorage.removeItem('name'); 
+    localStorage.removeItem('name'); // Clear the token from localStorage
     navigate('/', { replace: true }); // Redirect to login and prevent back button issues
   };
   useEffect(()=>{
