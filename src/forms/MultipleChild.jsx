099451def74e51b1868e79ba9121656f0bd2ae57
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-09-18 18:35:14 +0530

    Update MultipleChild

--- src/forms/MultipleChild.jsx
+++ src/forms/MultipleChild.jsx
@@ -291,11 +291,11 @@ function MultipleChild() {
             const resultData = await getResult.json();
             if (!getResult.ok) throw new Error('Failed to fetch results');  
                 // console.log(resultData);
-               
+               setCalculationResult(resultData.data[0]);
                 console.log(`Result: ${resultData.data[0].result}`);
                 toast.success(`Result: ${resultData.data[0].result}`);
-                setnewResult(resultData.data[0].result);
-                setCalculationResult(resultData.data[0].result);
+                // setnewResult(resultData.data[0].result);
+                // setCalculationResult(resultData.data[0].result);
                 // console.log(calculationResult);
                 handleReset();
               // console.log(calculationNetpay); // Set result in state
@@ -579,7 +579,7 @@ return (
             {newresult  &&   (
                 <div className="result-section">
                   {/* <h3>Calculation Result:</h3> */}
-                  <p>Result: {newresult}</p>
+                  <p>Result: {calculationResult.result}</p>
                 </div>
             )}
 
