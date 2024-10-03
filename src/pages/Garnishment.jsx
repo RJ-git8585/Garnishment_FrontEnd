fd236895fed9c1d8d9b3357785332ad482615ad9
Sourabhkostiodt <sourabh.kosti@orangedatatech.com>
2024-10-03 18:18:25 +0530

    ejwkej

--- src/pages/Garnishment.jsx
+++ src/pages/Garnishment.jsx
@@ -144,7 +144,7 @@ tabs Section  */}
       {/* Tab headers */}
       <div className="tabs">
         <button 
-          className={activeTab === 'Child Support' ? 'active mb zm aal aek aki arw asi awu axc bbe bck bjp bpv bpw bpy bqf' : ''} 
+          className={activeTab === 'Child Support' ? 'active inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 -my-2.5 ml-8' : ''} 
           onClick={() => handleTabClick('Child Support')}>
          Child Support
         </button>
