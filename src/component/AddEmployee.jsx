177bbc7dd36e9ee7617f9c5ffe0445fadc0563d2
Sourabhkostiodt <sourabh.kosti@orangedatatech.com>
2024-12-24 16:47:41 +0530

    Issue fixed

--- src/component/AddEmployee.jsx
+++ src/component/AddEmployee.jsx
@@ -254,7 +254,6 @@ const handleReset = () => {
                               value={number_of_child_support_order}
                               onChange={(e) => setNumberGarnihsment(e.target.value)}    
                               // autoComplete="current-password"
-                              
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
 
