83d4349a627a0079b9f7234082d52cb67ac58c74
Sourabhkostiodt <sourabh.kosti@orangedatatech.com>
2025-05-23 18:16:38 +0530

    Docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-// eslint-disable-next-line no-unused-vars
+
+/**
+ * The main App component serves as the root component of the application.
+ * It imports and applies global styles and renders the `RoutesPath` component,
+ * which is responsible for managing the application's routing.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered App component.
+ */
 import React, { useState, useEffect } from 'react';
 import './utils/main-css/App.css'
 import RoutesPath from './component/RoutesPath';
